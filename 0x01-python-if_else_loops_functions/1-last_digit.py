9ca9873e735468f8a94abb7026d2d01e703abd8b
Michael Bekele <shiferawmichael3@gmail.com>
2022-08-23 13:05:47 -0700

    task 1 fix 7

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,14 +3,14 @@ import random
 number = random.randint(-10000, 10000)
 num_to_str = repr(number)
 last_digit_str = num_to_str[-1]
-lastd = int(last_digit_str)
-if number > 0 and lastd > 5:
-    print("Last digit of", number, "is", lastd, "and is greater than 5")
-elif lastd == 0:
-    print("Last digit of", number, "is", lastd, "and is 0")
-elif number > 0 and lastd < 6:
-    print("Last digit of", number, "is", lastd, "and is less than 6 and not 0")
+la = int(last_digit_str)
+if number > 0 and la > 5:
+    print("Last digit of", number, "is", la, "and is greater than 5\n")
+elif la == 0:
+    print("Last digit of", number, "is", la, "and is 0\n")
+elif number > 0 and la < 6:
+    print("Last digit of", number, "is", la, "and is less than 6 and not 0\n")
 elif number < 0:
-    print("Last digit of", number, "is", -lastd, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", -la, "and is less than 6 and not 0\n")
 elif number < 6 and number < 0:
-    print("Last digit of", number, "is-", -lastd, "and is less than 6 and not 0")
+    print("Last digit of", number, "is", -la, "and is less than 6 and not 0\n")
