8586ae1fe492ec98c400cc9553d6b05b937004a7
Michael Bekele <shiferawmichael3@gmail.com>
2022-08-23 13:07:34 -0700

    task 1 fix 8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,12 +5,12 @@ num_to_str = repr(number)
 last_digit_str = num_to_str[-1]
 la = int(last_digit_str)
 if number > 0 and la > 5:
-    print("Last digit of", number, "is", la, "and is greater than 5\n")
+    print("Last digit of", number, "is", la, "and is greater than 5")
 elif la == 0:
-    print("Last digit of", number, "is", la, "and is 0\n")
+    print("Last digit of", number, "is", la, "and is 0")
 elif number > 0 and la < 6:
-    print("Last digit of", number, "is", la, "and is less than 6 and not 0\n")
+    print("Last digit of", number, "is", la, "and is less than 6 and not 0")
 elif number < 0:
-    print("Last digit of", number, "is", -la, "and is less than 6 and not 0\n")
+    print("Last digit of", number, "is", -la, "and is less than 6 and not 0")
 elif number < 6 and number < 0:
     print("Last digit of", number, "is", -la, "and is less than 6 and not 0\n")
