c20858fb43ea950638a2be090f0c4a2b4628f76b
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-02 07:28:23 -0700

    10th challenge

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,11 @@
+#include "LISTS_H"
+/*check_cycle- This function checks if a singly list has a cycle in it
+ * int- intiger
+ * listint_t- list integer
+ * Return: returns zero or 1
+ */
+
+int check_cycle(listint_t *list)
+{
+}
+
