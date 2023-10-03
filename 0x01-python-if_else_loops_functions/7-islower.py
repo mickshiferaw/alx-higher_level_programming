d2cd43cf6d1eaed87417a96c4dc9aab18b4e7748
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-03 13:38:12 -0700

    8th fix5

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,6 @@
 def islower(c):
     data = (ord(c))
     if data > 96 and data < 124:
-        print(f"{c} is lower")
         return (True)
     else:
-        print(f"{c} is upper")
         return (False)
