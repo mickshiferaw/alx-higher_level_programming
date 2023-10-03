a3c9cce3d9662c107d849d7d42a98900346be6cf
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-03 12:35:17 -0700

    10th fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    num_st = repr(number)
+num = int(input("please inter a number: "))
+def print_last_digit(num):
+    num_st = repr(num)
     last_str = num_st[-1]
     last_digit = int(last_str)
     return (last_digit)
