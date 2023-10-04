f51c39078f5c6504f3022db69cbee1879498f621
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-04 08:26:18 -0700

    adv fixed

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for m in reversed(range(97,123)):
+for m in reversed(range(97, 123)):
     if m % 2 == 0:
         print('{}'.format(chr(m  )), end="")
-    else:    
+    else:
         print('{}'.format(chr(m - 32)), end="")
