b33002fbccbada1de489c709019778acdb754476
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-05 09:55:35 -0700

    fixxxx3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-count = len(sys.argv) - 1
-if count == 1:
-    print("1 argument:")
-else:
-    print('{} arguments:'.format(count))
-for m in range(count):
+    count = len(sys.argv) - 1
+    if count == 1:
+        print("1 argument:")
+    else:
+        print('{} arguments:'.format(count))
+    for m in range(count):
         print("{}: {}".format(m + 1, sys.argv[m + 1]))
