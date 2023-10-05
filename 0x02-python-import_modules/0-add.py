34e4662dea5f83a6b4dd12b14005bf00fb956b2e
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-05 05:46:44 -0700

    0fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import add_0
-    def add(a, b):
-        a= 1
-        b= 2
-        return a + b
+import add_0
+def add(a, b):
+    a= 1
+    b= 2
+    return a + b
 print('a + b = ''{}'.format(add(1, 2)))
