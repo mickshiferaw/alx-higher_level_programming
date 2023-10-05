d30fec20a3c2439cef087e558f41d3dac4e9bd8f
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-05 09:48:12 -0700

    fixxxx1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,5 +5,5 @@ if count == 1:
     print("1 argument:")
 else:
     print('{} arguments:'.format(count))
-for m in range(count): 
+for m in range(count):
         print("{}: {}".format(m + 1, sys.argv[m + 1]))
