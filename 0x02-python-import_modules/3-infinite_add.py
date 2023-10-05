f44fa9a20b71a234e1c2e8a0778008326f126795
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-05 12:11:49 -0700

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     add = 0
-    count = len(sys.argv) - 1
-    for m in range(count):
-        print(int(m + add))
+    for i in range(len(sys.argv) - 1):
+        add = add + int(sys.argv[i + 1])
+    print('{}'.format(add))
