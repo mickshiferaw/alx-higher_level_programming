922006d6239d748f00b7aff01fc6de2b324d3128
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-07 03:08:51 -0700

    task1final1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or int(idx) > len(my_list) - 1:
-        return('none')
+        print('none')
     else:
         print("Element at index {:d} is {}".format(idx, my_list[idx]))
