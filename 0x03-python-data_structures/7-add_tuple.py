678d4e6685779a4e17dbef887d7c803299d2352a
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-08 07:04:32 -0700

    fix7d

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,13 +2,19 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     newA = list(tuple_a)
     newB = list(tuple_b)
+    if len(newA) == 0 and len(newB) == 0:
+        newA.append(0)
+        newB.append(0)
+        add1 = newA[0] + newB[0]
+        add2 = newA[0] + newB[0]
+        return ((add1, add2))
     if len(newA) < 1:
         newA.append(0)
         add1 = newA[0] + newB[0]
         add2 = newA[0] + newB[1]
         return ((add1, add2))
     if len(newB) < 1:
-        newA.append(0)
+        newB.append(0)
         add1 = newA[0] + newB[0]
         add2 = newA[1] + newB[0]
         return ((add1, add2))
