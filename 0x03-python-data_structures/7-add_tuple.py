6b62b8ae77beecd5ecb7aff667a3ff8682fa1592
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-08 04:55:17 -0700

    task7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    newA = list(tuple_a)
+    newB = list(tuple_b)
+    if len(newA) < 2 or len(newB) < 2:
+        newA.append(0)
+        newB.append(0)
+    if len(newA) > 2 or len(newB) > 2:
+        tuppleAdd1 = newA[0] + newB[0]
+        tuppleAdd2 = newA[1] + newB[1]
+    return ((tuppleAdd1,tuppleAdd2))
