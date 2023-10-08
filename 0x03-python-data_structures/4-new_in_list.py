9727fd0475b2fd9431a01c5b1de48090a5d68ce8
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-08 02:34:32 -0700

    fix4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if idx < 0 or idx > (len(my_list)) - 1:
-        return (new_list)
+        print (new_list)
     else:
         new_list[idx] = element
-        return (new_list)
+        print (new_list)
