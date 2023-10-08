537aafb1b2c7ad30be68f791d1d081a58b004d2a
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-08 02:55:21 -0700

    fix5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    finalStr = ''
     for x in my_string:
         if x == 'c' or x== 'C':
             x=''
-        print(x, end='')
-    print()
+        finalStr+=str(x)
+    return(finalStr)
