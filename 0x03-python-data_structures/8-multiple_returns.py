410ebffeb45f4bcc7ac448ee083c88ba8500ac99
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-08 07:30:04 -0700

    task8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == '':
+        return("First character: {}".format(None))
+    else:
+        fSentence = sentence[0]
+        length = len(sentence)
+        return("Length: {:d} - First character: {}".format(None, fSentence))
