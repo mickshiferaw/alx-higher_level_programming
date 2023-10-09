8062b91447dca300949180e3144c96fcbb8f515a
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-09 06:23:55 -0700

    fix8a

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
+def multiple_returns(sentence=''):
     if len(sentence) == 0:
         return (None)
     else:
-        fSentence = sentence[0]
+        first = sentence[0]
         length = len(sentence)
-        return ((f"({length}, {fSentence})"))
+        return ((length, first))
