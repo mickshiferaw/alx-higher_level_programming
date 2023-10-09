ec5eda56e441cad9aa8cfd16b63921d156916349
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-09 11:46:12 -0700

    task6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for inMatrix in matrix:
+        if len(inMatrix) < 1:
+            print()
+        for x in range(len(inMatrix)):
+            print('{:d}'.format(inMatrix[x]), end='\n' if x is len(inMatrix) - 1 else ' ')
