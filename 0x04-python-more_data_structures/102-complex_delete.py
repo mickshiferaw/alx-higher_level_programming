d9b7b71859e76f9b84e5c8a24a215125526f531a
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-11 12:20:52 -0700

    102fix

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,4 +4,4 @@ def complex_delete(a_dictionary, value=''):
     for x in listKy:
         if value == a_dictionary.get(x):
             del a_dictionary[x]
-        return (a_dictionary)
+    return (a_dictionary)
