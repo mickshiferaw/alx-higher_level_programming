acc0366a4ceea191bb6410ce4655df0185d8cf7f
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-11 04:05:12 -0700

    fix1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if not my_list:
+        return
     for x, y in enumerate(my_list):
         if y == search:
             my_list[x] = replace
