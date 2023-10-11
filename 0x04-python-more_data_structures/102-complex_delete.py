ccc3e27e10dfefd6223f156ae14596f3ae529219
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-11 09:25:27 -0700

    advanced2

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value=''):
+    newdict = a_dictionary.copy()
+    if not value:
+        return a_dictionary
+    for x, y in a_dictionary.items():
+        if value == x:
+            del newdict[x]
+    for k, m in newdict.items():
+        return (k, m)
