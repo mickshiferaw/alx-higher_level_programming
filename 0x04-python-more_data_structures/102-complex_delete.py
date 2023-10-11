08c3d61854761d1f8c9a6e4c14ee78a3ceb53521
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-11 12:17:28 -0700

    102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value=''):
-    newdict = a_dictionary.copy()
-    if not value:
-        return a_dictionary
-    for x, y in a_dictionary.items():
-        if value == x:
-            del newdict[x]
-    for k, m in newdict.items():
-        print(k, m)
+    listKy = list(a_dictionary.keys())
+    for x in listKy:
+        if value == a_dictionary.get(x):
+            del a_dictionary[x]
+        return (a_dictionary)
