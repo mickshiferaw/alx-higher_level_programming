432c059bbf9174f2dcf4a684355aa53f1a9eb2b2
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-23 10:08:56 -0700

    fix2 of 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return (True)
     except ValueError:
         print("{} is not an integer".format(value))
+        return (False)
     return (value)
