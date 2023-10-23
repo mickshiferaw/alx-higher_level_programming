dc7d4f745644abc4a72872e1b499f56ce109e0ac
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-23 07:16:41 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for y in my_list:
+            if my_list[y-1] <= x:
+                print(y, end='')
+        print()
+    except ValueError:
+        print("fix some errors")
