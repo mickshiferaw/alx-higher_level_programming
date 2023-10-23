c808ad307a0d98f5dc43a4be6e6d0cca7d4c136e
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-23 13:34:06 -0700

    advanced1fix2

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,8 +2,7 @@
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return (True)
     except ValueError as a:
         print("Exception: {}".format(a))
         return (False)
-    else:
-        return (True)
