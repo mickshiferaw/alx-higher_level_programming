631ce9bd6ce6c6733b55f44ddaea9623528e1203
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-23 12:21:39 -0700

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    newList = [val for val in my_list if isinstance(val, int)]
+    count = 0
+    for y in newList:
+        try:
+            if newList[y-1] <= x:
+                print(y, end="")
+                count += 1
+        except IndexError:
+            return
+    print()
+    return (count)
