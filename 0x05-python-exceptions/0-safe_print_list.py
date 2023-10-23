d3731fe49b318f76d3a65b4856a1bbdbb3b17062
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-23 07:45:38 -0700

    fix2 for 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for y in my_list:
+    for y in my_list:
+        try:
             if my_list[y-1] <= x:
-                return (y)
-    except ValueError:
-        print("fix some errors")
+                print(y, end='')
+        except IndexError:
+            print("fix some errors")
+            break
+    print()
