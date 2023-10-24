d75ef5a7bfdd1d51857c5e24fd57aa7faa36fa77
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-24 08:08:45 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" define a class named square.
+    @size is a private attribute.
+"""
+
+
+class Square:
+    """
+    this is a first class task.
+    this class doesnt do anything.
+    @size is a private attribute.
+    """
+    def __init__(self, size):
+        self.__size = size
