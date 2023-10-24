7ccb17e2453b788b6d195ae02a37f8d1d52ad4e4
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-24 07:30:50 -0700

    task0fix0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class Square:
     """
-    this is a first class task
-    this class doesnt do anything
-    its just an empty class
-    the word pass is used bcz
-    in class there must be at least one line
+    this is a first class task.
+    this class doesnt do anything.
+    its just an empty class.
+    the word pass is used bcz.
+    in class there must be at least one line.
     """
     pass
