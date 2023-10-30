9aef17884132800bcfea5845ceea3ac1c8c0dbf2
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-10-30 06:39:04 -0700

    fix11

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -22,6 +22,8 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
     """this is the getter for height"""
     @property
     def height(self):
@@ -33,3 +35,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
