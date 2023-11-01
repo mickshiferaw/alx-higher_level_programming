24f228073ee4849cddd9bedd2f221749dec82414
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-11-01 16:28:06 -0700

    task101f1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""this is an empty class"""
+
+
 class LockedClass:
     """prevents the user from dynamically creating new instance attributes"""
     __slots__ = ["first_name"]
