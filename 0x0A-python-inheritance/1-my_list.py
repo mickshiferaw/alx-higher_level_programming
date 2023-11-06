b2f1e9f70a59f92b7c12dc119cea3da00f917279
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-11-06 05:56:07 -0800

    fix5of1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,7 @@
 
 
 class MyList(list):
+    """a class that inherits from list"""
     def print_sorted(self):
         """sorted method from a list will not change the original list"""
         print(sorted(self))
