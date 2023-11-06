3ea8d4c12405f03243bd237a1ff9df288a699b0d
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-11-06 05:53:56 -0800

    fix4of1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,10 +3,6 @@
 
 
 class MyList(list):
-    """we can call the super class, but its unnessesary in this case
-    def __init__(self):
-        list.__init__(self)
-    """
     def print_sorted(self):
         """sorted method from a list will not change the original list"""
         print(sorted(self))
