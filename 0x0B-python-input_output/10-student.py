21549344c8385e785ff9ee2086ef019747d196c2
michael bekele shiferaw <shiferawmichael3@gmail.com>
2023-11-07 11:50:19 -0800

    task10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a class representing a student"""
+
+
+class Student:
+    """a class representing a student
+    Args:
+        first_name: student first name
+        last_name: student last name
+        age: student age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+        Args:
+            attrs: attribute
+        """
+        if (type(attrs) == list and
+                all(type(eleme) == str for eleme in attrs)):
+            return {m: getattr(self, m) for m in attrs if hasattr(self, m)}
+        return self.__dict__
